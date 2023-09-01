0a6ad8e9bdc1ecaa4d2f4628ae98e74eab25e099
Iamneo <techadmin@iamneo.ai>
2023-09-01 06:33:17 +0000

    Backup Commit

--- Apptest.java
+++ Apptest.java
@@ -1,3 +1,38 @@
+package com.example.tests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 public class Apptest {
-    
+    ChromeDriver driver;
+    String url = "http://www.flipkart.com";
+    public void invokeBrowser() {
+       System.setProperty("webdriver.chrome.driver",
+          "C:\\Users\\Ritika Bharadwaj\\workspace\\libs\\chromeDriver36\\chromedriver.exe");
+       driver = new ChromeDriver();
+       driver.manage().window().maximize();
+       driver.manage().deleteAllCookies();
+       driver.manage().timeouts().pageLoadTimeout(90, TimeUnit.SECONDS);
+       driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+       driver.get(url);
+    }
+    public void getLinkCount() {
+        List<WebElement> allLink = driver.findElements(By.tagName("a"));
+        System.out.println("Number of links on a page :: " + allLink.size());
+    }
+    public void getLinkUrl() {
+        String url = driver.findElement(By.linkText("Amazon Pay")).getAttribute("href");
+        System.out.println("Url :: " + url);
+    }
+    public void getAllLinkInfo() {
+        List<WebElement> allLink = driver.findElements(By.tagName("a"));
+        for (WebElement link : allLink) {
+            System.out.println("Link Text :: " + link.getText());
+. ext());
+System.out.println("Link URL :: " + link.getAttribute("href"));
+System.out.println("-------------------------------------------");
 }
+}
+}
+
